2b1ae55fe7c8f1648b26e80ad1616cd5dc97309c
Nqweuijkl-Neil <neilpatrickmorano@gmail.com>
2026-03-04 23:01:32 +0800

    fix: use HTTPS for all OMDb API requests

--- src/App.jsx
+++ src/App.jsx
@@ -244,7 +244,7 @@ function MovieDetails({
         async function getMovieDetails() {
             setIsLoading(true);
             const res = await fetch(
-                `http://www.omdbapi.com/?apikey=${apiKey}&i=${selectedId}`,
+                `https://www.omdbapi.com/?apikey=${apiKey}&i=${selectedId}`,
             );
             const data = await res.json();
             setMovie(data);
